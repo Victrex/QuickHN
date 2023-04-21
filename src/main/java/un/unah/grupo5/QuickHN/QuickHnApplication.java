2cb480cf86a21e3eb872dd11343e663ecb971699
Kenneth Pineda <kennethpineda11@gmail.com>
2023-04-21 10:11:46 -0600

    creacion de proyecto springboot

---
+++ src/main/java/un/unah/grupo5/QuickHN/QuickHnApplication.java
@@ -0,0 +1,13 @@
+package un.unah.grupo5.QuickHN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickHnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickHnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/un/unah/grupo5/QuickHN/QuickHnApplicationTests.java
@@ -0,0 +1,13 @@
+package un.unah.grupo5.QuickHN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickHnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
