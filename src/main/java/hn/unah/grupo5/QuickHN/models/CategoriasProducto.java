078448a8018d78a7d164ee8ea32abfd3634a6a71
Eduardo Soriano <EDuardDOsm.13@gmail.com>
2023-04-21 16:28:32 -0600

    update models

---
+++ src/main/java/hn/unah/grupo5/QuickHN/models/CategoriasProducto.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hn.unah.grupo5.QuickHN.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Soriano
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="CategoriasProducto")
+public class CategoriasProducto implements Serializable{
+    @Id
+    private String idcategoriaproducto;
+    private String nombre;
+    private String descripcion;
+    
+    private Imagenes idimagen;
+}
---
+++ src/main/java/hn/unah/grupo5/QuickHN/models/Dimensiones.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hn.unah.grupo5.QuickHN.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Soriano
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="Dimensiones")
+public class Dimensiones implements Serializable{
+    @Id
+    private String iddimension;
+    private int alto;
+    private int ancho;
+    private int largo;
+}
