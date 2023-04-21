27dbba34e56419b58259f1f758409c256620f9d7
cairocen <109777230+cairocen@users.noreply.github.com>
2023-04-21 16:02:55 -0600

    Modelos de tablas restantes

---
+++ src/main/java/hn/unah/grupo5/QuickHN/models/MetodosPago.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hn.unah.grupo5.QuickHN.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/**
+ *
+ * @author mcenteno
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="MetodosPago")
+public class MetodosPago {
+    @Id
+    private String idmetodopago;
+    
+    @Column(name="tipometodo")
+    private String tipometodo;
+    
+}
