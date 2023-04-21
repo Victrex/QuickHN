1dd4dc7bf560d1622bdf946fc6942e7a6d42ea1d
Kenneth Pineda <kennethpineda11@gmail.com>
2023-04-21 16:48:09 -0600

    models

---
+++ src/main/java/hn/unah/grupo5/QuickHN/models/ProductosPalabrasClave.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hn.unah.grupo5.QuickHN.models;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Kenne
+ */
+public class ProductosPalabrasClave implements Serializable{
+    //Pendiente clase para id compuesta
+}
