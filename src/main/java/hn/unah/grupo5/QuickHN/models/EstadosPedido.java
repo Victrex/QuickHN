046fccf4f05441e555032fbc897389043e475bb7
cairocen <109777230+cairocen@users.noreply.github.com>
2023-04-21 16:19:17 -0600

    Modelos EstadosPedido y Pedidos

---
+++ src/main/java/hn/unah/grupo5/QuickHN/models/EstadosPedido.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hn.unah.grupo5.QuickHN.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/**
+ *
+ * @author mcenteno
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="EstadosPedido")
+public class EstadosPedido implements Serializable {
+    @Id
+    private String idestadopedido; //Identificar Relación
+    
+    @Column(name="estado")
+    private String estado;
+
+}
