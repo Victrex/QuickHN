104187600541414584222dfcf6037124744521f1
Kenneth Pineda <kennethpineda11@gmail.com>
2023-04-22 14:40:07 -0600

    new packages

---
+++ src/main/java/hn/unah/grupo5/QuickHN/controllers/ProductosController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hn.unah.grupo5.QuickHN.controllers;
+
+/**
+ *
+ * @author Pinedas
+ */
+public class ProductosController {
+    
+}
