f842a0a2f29f92a6e4fbaf8430497ba95a1a80ba
Eduardo Soriano <EDuardDOsm.13@gmail.com>
2023-04-22 22:43:08 -0600

    controller update

--- src/main/java/hn/unah/grupo5/QuickHN/DTOs/CategoriasProductoDTO.java
+++ src/main/java/hn/unah/grupo5/QuickHN/DTOs/CategoriasProductoDTO.java
@@ -17,7 +17,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoriasProductoDTO {
-
     @JsonProperty
     private String idcategoriaproducto;
     @JsonProperty
